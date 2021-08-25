c4e07b1c11e51785ad0fbf64050f9e3409766aa3
codingscode <heronnestg@hotmail.com>
2021-08-24 22:39:38 -0300

    parte 8

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -4,37 +4,24 @@ import { View, Text, Image } from 'react-native'
 
 
 class App extends Component {
-  render() {
-     return (
-        <View>
-           <Text>mais testes!</Text>
-           <Text style={{ color: 'blue', fontSize: 25, margin: 15 }} >
-              Sujeito Programador
-           </Text>
-           <Jobs largura={80} altura={50} nome="stevie" />
-
-        </View>
-     )
-  }
+   constructor(props) {
+      super(props)
+      this.state = {
+         nome: 'Simon'
+      }
+   }
+ 
+ 
+   render() {
+      return (
+         <View>
+            <Text>{this.state.nome}</Text>
+ 
+         </View>
+      )
+   }
 }
 
 
 export default App
-
-
-class Jobs extends Component {
-  
-  render() {
-    const imagem = 'https://sujeitoprogramador.com/steve.png'
-
-
-     return (
-        <View>
-          <Image source={{ uri: imagem }} style={{width: this.props.largura, height: this.props.altura}} />
-          <Text>{this.props.nome}</Text>
-
-        </View>
-     )
-  }
-}
 
