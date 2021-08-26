1d4dda9eebb2608ed4efbc31d6216ef09599a52f
codingscode <heronnestg@hotmail.com>
2021-08-25 22:08:15 -0300

    parte 12

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,36 +1,29 @@
 import React, { Component } from 'react'
-import { View, Text, Image, Button } from 'react-native'
-
-
+import { View, Text, Image, Button, StyleSheet } from 'react-native'
 
 
 
 class App extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-         nome: 'Simon'
-      }
+   
 
-      this.entrar = this.entrar.bind(this)
-   }
- 
-   entrar(valor) {
-      this.setState({
-        nome: valor
-      })
-   }
+
  
    render() {
       return (
-         <View style={{ marginTop: 20 }} >
-            <Button title="entrar" onPress={() => this.entrar('Jack')} />
-            <Text style={{ fontSize: 23, color: 'red', textAlign: 'center' }} >{this.state.nome}</Text>
- 
+         <View style={estilos.area} >
+            <Text>texto 1</Text>
+            <Text>texto 2</Text>
+            <Text>texto 3</Text>
+            <Text>texto 4</Text>
+            
          </View>
       )
    }
 }
+
+const estilos = StyleSheet.create({
+   area: { marginTop: 40 }
+})
 
 
 export default App
