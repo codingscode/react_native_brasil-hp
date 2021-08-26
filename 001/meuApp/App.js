7d6b2483d54f85fa4dd65c4e0492ac2ca7ab81cd
codingscode <heronnestg@hotmail.com>
2021-08-25 22:28:21 -0300

    parte 17

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -11,8 +11,9 @@ class App extends Component {
    render() {
       return (
          <View style={{ backgroundColor: 'orange', flex: 1}} >
-            <View style={{flex: 1, width: 50, height: 50, backgroundColor: 'green'}} ></View>   
-            <View style={{flex: 1, width: 50, height: 50, backgroundColor: 'purple'}} ></View>   
+            <View style={{flex: 2, backgroundColor: 'lightgreen'}} ></View>   
+            <View style={{flex: 1, backgroundColor: 'purple'}} ></View>   
+            <View style={{flex: 1, backgroundColor: 'yellow'}} ></View>   
             
          </View>
       )
