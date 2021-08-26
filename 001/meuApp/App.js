69b8a9bb0d1fc03af73d6578eaaa3461a3014821
codingscode <heronnestg@hotmail.com>
2021-08-26 20:13:54 -0300

    parte 33

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button, Image } from 'react-native'
 
 
 
 class App extends Component {
    
-   constructor(props) {
-      super(props)
-      this.state = {
-         nome: '', entrada: ''
-      }
-
-      this.entrar = this.entrar.bind(this)
-
-   }
+  
    
-   entrar() {
-      if (this.state.entrada === '') {
-         alert('deve digitar algo')
-         return
-      }
-      this.setState({nome: this.state.entrada})   
-   }
-
+  
    
    
    render() {
       return (
          <View style={estilos.container}  >
-            <TextInput style={estilos.entrada} placeholder="digite algo" underLineColorAndroid="transparent" onChangeText={(texto) => this.setState({entrada: texto})} />
-            <Button title="Entrar" onPress={this.entrar} />
-            <Text style={estilos.texto} >{this.state.nome}</Text>
+            <Image source={require('./src/biscoito.png')} style={estilos.imagem} />
 
          </View>
       )
@@ -41,11 +24,8 @@ const estilos = StyleSheet.create({
    container: {
       flex: 1
    },
-   entrada: {
-      height: 45, borderWidth: 1, borderColor: '#222', margin: 10, fontSize: 20, padding: 10
-   },
-   texto: {
-      textAlign: 'center', fontSize: 25
+   imagem: {
+      width: 250, height: 250
    }
 })
 
