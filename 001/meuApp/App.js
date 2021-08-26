c16388206fa80e57a51b341d298cad8f85e10bf4
codingscode <heronnestg@hotmail.com>
2021-08-25 22:44:43 -0300

    parte 23

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -10,7 +10,7 @@ class App extends Component {
  
    render() {
       return (
-         <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center'}} >
+         <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between'}} >
             <View style={{width: 50, height: 50, backgroundColor: 'lightgreen'}} ></View>   
             <View style={{width: 50, height: 50, backgroundColor: 'purple'}} ></View>   
             <View style={{width: 50, height: 50, backgroundColor: 'yellow'}} ></View>   
