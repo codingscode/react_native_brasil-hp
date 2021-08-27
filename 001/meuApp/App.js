9a1c3fe20f31225016a1a68927559e41d2473822
codingscode <heronnestg@hotmail.com>
2021-08-27 20:54:31 -0300

    parte 42

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -4,48 +4,28 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from 'reac
 
 
 class App extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-         textoFrase: '',
-         imagem: require('./src/biscoito.png')
-      }
-
-      this.frases = [
-            'Siga os bons e aprenda com eles.', 
-            'O bom-senso vale mais do que muito conhecimento.', 
-            'O riso é a menor distância entre duas pessoas.', 
-            'Deixe de lado as preocupações e seja feliz.',
-            'Realize o óbvio, pense no improvável e conquiste o impossível.',
-            'Acredite em milagres, mas não dependa deles.',
-            'A maior barreira para o sucesso é o medo do fracasso.'
-      ]
-
-      this.quebraBiscoito = this.quebraBiscoito.bind(this)
-
-   }
    
-   quebraBiscoito() {
-      let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+   
 
-      this.setState({
-         textoFrase: `"${this.frases[numeroAleatorio]}"`, 
-         imagem: require('./src/biscoitoAberto.png')
-
-      })
-   }
    
    render() {
       return (
          <View style={estilos.container}  >
-            <Image source={this.state.imagem} style={estilos.imagem} />
-            <Text style={estilos.textoFrase} >{this.state.textoFrase}</Text>
-            <TouchableOpacity style={estilos.botao} onPress={this.quebraBiscoito} >
-               <View style={estilos.btnArea} >
-                  <Text style={estilos.btnTexto} >quebrar biscoitos</Text>
-               </View>   
-            </TouchableOpacity>            
-
+            <Image source={require('./src/cronometro.png')} style={estilos.cronometro} />
+            <Text style={estilos.timer} >0.0</Text>            
+            
+            <View style={estilos.btnArea} >
+              <TouchableOpacity style={estilos.btn} >
+                  <Text style={estilos.btnTexto} >
+                     vai
+                  </Text>
+               </TouchableOpacity>
+              <TouchableOpacity style={estilos.btn} >
+                  <Text style={estilos.btnTexto} >
+                     limpar
+                  </Text>
+               </TouchableOpacity>
+            </View>
          </View>
       )
    }
@@ -57,23 +37,9 @@ class App extends Component {
 
 const estilos = StyleSheet.create({
    container: {
-      flex: 1, alignItems: 'center', justifyContent: 'center'
-   },
-   imagem: {
-      width: 250, height: 250
-   },
-   textoFrase: {
-      fontSize: 20, color: '#dd7b22', margin: 30, fontStyle: 'italic', textAlign: 'center'
-   },
-   botao: {
-      width: 230, height: 50, borderWidth: 2, borderColor: '#dd7b22', borderRadius: 25
-   },
-   btnArea: {
-      flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'
-   },
-   btnTexto: {
-      fontSize: 18, fontWeight: 'bold', color: 'green'
+      flex: 1
    }
+
 })
 
 
