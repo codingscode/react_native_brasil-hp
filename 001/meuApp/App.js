81d7a72d6543dc49eb959a99d7b4fc6a80728497
codingscode <heronnestg@hotmail.com>
2021-08-27 22:13:16 -0300

    parte 55

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -4,67 +4,21 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from 'reac
 
 
 class App extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-        numero: 0, botao: 'vai', ultimo_tempo: null
-      }
    
-      this.timer = null
-      
-      this.vai = this.vai.bind(this)
-      this.limpar = this.limpar.bind(this)
-   }
 
-   vai() {
-      if (this.timer != null) {
-         clearInterval(this.timer)
-         this.timer = null
-         this.setState({botao: 'vai'})
-      }
-      else {
-         this.timer = setInterval(() => {
-            this.setState({numero: this.state.numero + 0.1})
-         }, 100)
-         this.setState({botao: 'parar'})
-      }
    
-   }
    
-   limpar() {
-      if (this.timer != null) {
-         clearInterval(this.timer)
-         this.timer = null
-      }
-      this.setState({
-         numero: 0, botao: 'vai', ultimo_tempo: this.state.numero
-      })
-   }
+   
    
    
    render() {
       return (
          <View style={estilos.container}  >
-            <Image source={require('./src/cronometro.png')} style={estilos.cronometro} />
-            <Text style={estilos.timer} > {this.state.numero.toFixed(1)} </Text>            
+            <View style={estilos.box1} ></View>
+            <View style={estilos.box2} ></View>
+            <View style={estilos.box3} ></View>
+            <View style={estilos.box4} ></View>
             
-            <View style={estilos.btnArea} >
-              <TouchableOpacity style={estilos.btn} onPress={this.vai} >
-                  <Text style={estilos.btnTexto} >
-                     {this.state.botao}
-                  </Text>
-               </TouchableOpacity>
-              <TouchableOpacity style={estilos.btn} onPress={this.limpar} >
-                  <Text style={estilos.btnTexto} >
-                     limpar
-                  </Text>
-               </TouchableOpacity>
-            </View>
-            <View style={estilos.areaUltima} >
-               <Text style={estilos.textoCorrida} >
-               {this.state.ultimo_tempo > 0 ? `Último tempo: ${this.state.ultimo_tempo.toFixed(1)}s`: ''}
-               </Text>
-            </View>
          </View>
       )
    }
@@ -76,26 +30,21 @@ class App extends Component {
 
 const estilos = StyleSheet.create({
    container: {
-      flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'lightblue'
+      flex: 1
    },
-   timer: {
-      marginTop: -160, color: '#FFF', fontSize: 65, fontWeight: 'bold'
+   box1: {
+      backgroundColor: 'orange', height: 250
    },
-   btnArea: {
-      flexDirection: 'row', marginTop: 70, height: 40
+   box2: {
+      backgroundColor: 'lightgreen', height: 250
    },
-   btn: {
-      flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFF', height: 40, margin: 17, borderRadius: 9
+   box3: {
+      backgroundColor: 'lightblue', height: 250
    },
-   btnTexto: {
-      fontSize: 20, fontWeight: 'bold', color: 'lightblue'
-   },
-   areaUltima: {
-      marginTop: 40
-   },
-   textoCorrida: {
-      fontSize: 25, fontStyle: 'italic', color: '#FFF'
+   box4: {
+      backgroundColor: 'purple', height: 250
    }
+ 
 
 })
 
