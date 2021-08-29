b132f5ca941245dc455a14cf2a906f4f501cf514
codingscode <heronnestg@hotmail.com>
2021-08-28 21:54:02 -0300

    parte 66

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-
+import { Picker } from '@react-native-picker/picker'
 
 
 
@@ -12,9 +12,12 @@ class App extends Component {
       return (
          <View style={estilos.container} >
             <Text style={estilos.logo} >Menu Pizza</Text>
-            
-            
-            
+            <Picker>
+               <Picker.Item key={1} value={1} label="Calabresa" />
+               <Picker.Item key={2} value={2} label="Mussarela" />
+               <Picker.Item key={3} value={3} label="Frango" />
+            </Picker>
+                        
             <Text style={estilos.pizzas} >você escolheu: pizza calabresa</Text>
             <Text style={estilos.pizzas} >R$: 59,90</Text>
          </View>
