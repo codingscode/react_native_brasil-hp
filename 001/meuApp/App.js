c60f6c66ba9d8e5afb1ae4d86c35f5aeaa2aba23
codingscode <heronnestg@hotmail.com>
2021-08-28 23:07:53 -0300

    parte 78

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -19,7 +19,9 @@ class App extends Component {
 
       return (
          <View style={estilos.container} >
-            <Switch value={this.state.status} onValueChange={(valorSwitch) => this.setState({status: valorSwitch})} />
+            <Switch value={this.state.status} 
+                    onValueChange={(valorSwitch) => this.setState({status: valorSwitch})}
+                    thumbColor="purple" />
             <Text style={{textAlign: 'center', fontSize: 30}} >
                {this.state.status ? 'ligado' : 'desligado'}
             </Text>
