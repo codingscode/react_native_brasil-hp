fcacc562e536cfe099e85e3a80f9070f909e5e93
codingscode <heronnestg@hotmail.com>
2021-08-30 23:38:12 -0300

    parte 98

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,62 +1,16 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, TextInput } from 'react-native'
 
-import Lista from './src/Lista/Lista.js'
+
 
 
 class App extends Component {
    constructor(props) {
       super(props)
       this.state = {
-         feed: [
-            {
-              id: '1', 
-              nome: 'Lucas Silva', 
-              descricao: 'Mais um dia de muitos bugs :)', 
-              imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
-              imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
-              likeada: false, 
-              likers: 0
-            },
-            {
-              id: '2', 
-              nome: 'Matheus', 
-              descricao: 'Isso sim é ser raiz!!!!!', 
-              imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
-              imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
-              likeada: false, 
-              likers: 0
-            },
-            {
-              id: '3', 
-              nome: 'Jose Augusto', 
-              descricao: 'Bora trabalhar Haha', 
-              imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
-              imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
-              likeada: false, 
-              likers: 3
-            },
-            {
-              id: '4', 
-              nome: 'Gustavo Henrique', 
-              descricao: 'Isso sim que é TI!', 
-              imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
-              imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
-              likeada: false, 
-              likers: 1
-            },
-            {
-              id: '5', 
-              nome: 'Guilherme', 
-              descricao: 'Boa tarde galera do insta...', 
-              imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
-              imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
-              likeada: false, 
-              likers: 32
-            }
-          ]
-    
+         input: '', nome: ''
       }
+
    }
 
    
@@ -64,17 +18,12 @@ class App extends Component {
 
       return (
          <View style={estilos.container} >
-            <View style={estilos.cabecalho} >
+            <View>
+               <TextInput style={estilos.input} value={this.state.input} onChangeText={(texto) => this.setState({input: texto})} underLineColorAndroid="transparent" />
                <TouchableOpacity>
-                  <Image source={require('./src/image2/logo.png')} style={estilos.logo} />
+                  <Text style={estilos.botao} >+</Text>
                </TouchableOpacity>
-               <TouchableOpacity>
-                  <Image source={require('./src/image2/send.png')} style={estilos.enviar} />
-               </TouchableOpacity>
-
-            </View>
-            <FlatList keyExtractor={(item) => item.id} showsHorizontalScrollIndicator={false} 
-                      data={this.state.feed} renderItem={({item}) => <Lista data={item} />} />
+            </View>  
 
          </View>
       )
@@ -88,17 +37,6 @@ class App extends Component {
 const estilos = StyleSheet.create({
    container: {
       flex: 1
-   },
-   cabecalho: {
-      height: 55, backgroundColor: '#FFF', flexDirection: 'row', alignItems: 'center', 
-      justifyContent: 'space-between', padding: 5, borderBottomWidth: 0.2, shadowColor: '#000', 
-      elevation: 1
-   },
-   enviar: {
-      width: 23, height: 23
-   },
-   logo: {
-   
    }
   
 })
