e448073cb668bb29b240960aeb06b16a1b41c5a5
codingscode <heronnestg@hotmail.com>
2021-08-31 22:37:10 -0300

    parte 104

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity, Keyboard, TextInput } from 'react-native'
-
-import AsyncStorage from '@react-native-community/async-storage'
+import { View, Text, StyleSheet, Platform, Button, Modal } from 'react-native'
 
 
 
@@ -9,32 +7,19 @@ class App extends Component {
    constructor(props) {
       super(props)
       this.state = {
-         input: 'wqeqwe', nome: 'aldin'
+         modalVisivel: false
       }
 
-      this.gravaNome = this.gravaNome.bind(this)
+      this.entrar = this.entrar.bind(this)
+      this.sair = this.sair.bind(this)
    }
    
-   gravaNome() {
-      this.setState({
-         nome: this.state.input
-      })
-      alert('Salvo com sucesso!')
-      Keyboard.dismiss()  // garante que o teclado fecha
+   entrar() {
+      this.setState({modalVisivel: true})
    }
 
-   async componentDidMount() {
-      await AsyncStorage.getItem('nome').then((valor) => {
-         this.setState({nome: valor})
-      })
-   }
-   
-   async componentDidUpdate(_, prevState) {
-      const nome = this.state.nome
-      
-      if (prevState !== nome) {
-         await AsyncStorage.setItem('nome', nome)
-      }
+   sair(visivel) {
+      this.setState({modalVisivel: visivel})
    }
 
    
@@ -42,13 +27,14 @@ class App extends Component {
 
       return (
          <View style={estilos.container} >
-            <View style={estilos.viewInput} >
-               <TextInput style={estilos.input} value={this.state.input} onChangeText={(texto) => this.setState({input: texto})} underLineColorAndroid="transparent" />
-               <TouchableOpacity onPress={this.gravaNome} >
-                  <Text style={estilos.botao} >+</Text>
-               </TouchableOpacity>
-            </View>  
-            <Text style={estilos.nome} >{this.state.nome}</Text>
+            <Button title="Entrar" onPress={this.entrar} />
+            <Modal animation="slide" visible={this.state.modalVisivel} >
+               <View style={{backgroundColor: 'lightblue', flex: 1}} >
+                  <Text style={{color: 'orange', fontSize: 28}} >Seja Bem-vindo!</Text>
+                  <Button title="Sair" onPress={() => this.sair(false)} />
+               </View>
+            </Modal>
+
          </View>
       )
    }
@@ -60,20 +46,9 @@ class App extends Component {
 
 const estilos = StyleSheet.create({
    container: {
-      flex: 1, marginTop: 20, alignItems: 'center'
-   },
-   viewInput: {
-      flexDirection: 'row', alignItems: 'center'
-   },
-   input: {
-      width: 350, height: 40, borderColor: '#000', borderWidth: 1, padding: 10
-   },
-   botao: {
-      backgroundColor: '#222', color: '#FFF', height: 40, padding: 10, marginLeft: 4
-   },
-   nome: {
-      fontSize: 30, textAlign: 'center', marginTop: 15
+      flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#DDD'
    }
+
   
 })
 
