e841bbc098492de1a3e9db7dad4a7ca18e41567d
codingscode <heronnestg@hotmail.com>
2021-12-26 22:36:15 -0300

    parte 110

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Platform, Button, Modal } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 
-
-import Entrar from './src/Entrar/Entrar'
+import api from './src/services/api'
+import Filmes from './src/Filmes/Filmes'
 
 
 class App extends Component {
+
    constructor(props) {
       super(props)
+      
       this.state = {
-         modalVisivel: false
+         filmes: []
       }
-
-      this.entrar = this.entrar.bind(this)
-      this.sair = this.sair.bind(this)
-   }
+      
    
-   entrar() {
-      this.setState({modalVisivel: true})
    }
 
-   sair(visivel) {
-      this.setState({modalVisivel: visivel})
+   async componentDidMount() {
+      const response = await api.get('r-api/?api=filmes')
+      this.setState({
+         filmes: response.data
+      })
    }
 
    
@@ -29,29 +29,18 @@ class App extends Component {
 
       return (
          <View style={estilos.container} >
-            <Button title="Entrar" onPress={this.entrar} />
-            <Modal transparent={true} animationType="slide" visible={this.state.modalVisivel} >
-               <View style={{margin: 15, flex: 1, alignItems: 'center', justifyContent: 'center'}} >
-                  <Entrar sair={() => this.sair(false)} />
-
-               </View>
-            </Modal>
-
+            <Text>oie</Text>
+            <FlatList data={this.state.filmes} keyExtractor={item => item.id.toString()} renderItem={({item}) => <Filmes dados={item} /> } />
          </View>
       )
    }
 }
 
-/* 
-
-*/
-
 const estilos = StyleSheet.create({
    container: {
-      flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#DDD'
+      flex: 1
    }
 
-  
 })
 
 
