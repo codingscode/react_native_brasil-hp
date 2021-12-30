c0cbb13d3da19ac9c11fe158f6f23b62df504080
codingscode <heronnestg@hotmail.com>
2021-12-29 23:08:41 -0300

    parte 117

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 
+import Conversor from './src/Conversor/Conversor'
 
-
-
+// https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=217f52c14d405541a76e
 class App extends Component {
 
   
@@ -12,8 +12,8 @@ class App extends Component {
     
    render() {
       return (
-      <View  >
-         <Text>oie</Text>
+      <View style={estilos.container} >
+         <Conversor />
       </View>
 
       )
@@ -23,7 +23,7 @@ class App extends Component {
 
 const estilos = StyleSheet.create({
    container: {
-      flex: 1
+      flex: 1, justifyContent: 'center', alignItems: 'center'
    }
 
 })
---
+++ 001/meuApp/src/Conversor/Conversor.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+
+
+class Conversor extends Component {
+
+   render() {
+      return (
+      <View>
+        <Text>Conversor</Text>
+      </View>
+      )
+   }
+}
+
+export default Conversor
+
