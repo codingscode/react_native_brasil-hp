ae3752ddfc20fe27337727435a27b1e14c5058af
codingscode <heronnestg@hotmail.com>
2021-12-30 21:25:16 -0300

    parte 119

--- 001/meuApp/src/Conversor/Conversor.js
+++ 001/meuApp/src/Conversor/Conversor.js
@@ -21,10 +21,14 @@ class Conversor extends Component {
       return (
       <View style={estilos.container} >
         <Text style={estilos.titulo} >USD para BRL</Text>
-        <TextInput placeholder="Valor a ser convertido" style={estilos.areaEntrada} onChangeText={()=>{}} />
+        <TextInput placeholder="Valor a ser convertido" style={estilos.areaEntrada} 
+                   onChangeText={()=>{}} keyboardType='numeric' />
         <TouchableOpacity style={estilos.botaoArea} >
            <Text style={estilos.botaoTexto} >Converter</Text>
         </TouchableOpacity>
+        <Text style={estilos.valorConvertido} >
+           10.90
+        </Text>
       </View>
       )
    }
@@ -32,9 +36,25 @@ class Conversor extends Component {
 
 const estilos = StyleSheet.create({
    container: {
-   
+      flex: 1, justifyContent: 'center', alignItems: 'center'
+   },
+   titulo: {
+      fontSize: 30, fontWeight: 'bold', color: '#000'
+   },
+   areaEntrada: {
+      width: 280, height: 45, backgroundColor: '#CCC', textAlign: 'center', marginTop: 15, fontSize: 20, color: '#000', borderRadius: 5
+   },
+   botaoArea: {
+      width: 150, height: 45, backgroundColor: 'blue', borderRadius: 5, justifyContent: 'center', alignItems: 'center', marginTop: 15
+   },
+   botaoTexto: {
+      fontSize: 17, fontWeight: 'bold', color: '#FFF'
+   },
+   valorConvertido: {
+      fontSize: 30, fontWeight: 'bold', color: '#000', marginTop: 15
    }
    
+
    
 })
 
