af5808ba46f42877c7bacc1c01656f0476baea0a
codingscode <heronnestg@hotmail.com>
2022-01-02 21:27:15 -0300

    parte 125

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 
-import Conversor from './src/Conversor/Conversor'
 
-// https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=217f52c14d405541a76e
+
+
 class App extends Component {
 
+   constructor(props) {
+      super(props)
+      
+      this.state = {
+      
+      }
+      
+   }
   
     
    render() {
       return (
       <View style={estilos.container} >
-         <Conversor moedaA="USD" moedaB="BRL" />
+         <View style={{ width: 250, height: 250, backgroundColor: '#4169F1', justifyContent: 'center'}}>
+            <Text style={{ color: '#FFFFFF', fontSize: 22, textAlign: 'center' }} >Carregando...</Text>
+         </View>
       </View>
 
       )
@@ -24,6 +34,7 @@ const estilos = StyleSheet.create({
    container: {
       flex: 1, justifyContent: 'center', alignItems: 'center'
    }
+   
 
 })
 
