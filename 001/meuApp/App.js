1c55290431c93c61db4dcfc4f6deca0fc2b38bac
codingscode <heronnestg@hotmail.com>
2022-01-05 22:19:55 -0300

    parte 137

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Animated } from 'react-native'
+import { View, Text, StyleSheet, Animated, TouchableOpacity } from 'react-native'
 
 
 
@@ -11,36 +11,36 @@ class App extends Component {
       super(props)
       
       this.state = {
-         LarAnimada: new Animated.Value(150), AltAnimada: new Animated.Value(50), OpacidadeAnimada: new Animated.Value(0)
+         LarAnimada: new Animated.Value(150), AltAnimada: new Animated.Value(50), OpAnimada: 1
       }
       
+      this.carregarGrafico = this.carregarGrafico.bind(this)
+           
       
-      Animated.loop(
-         Animated.sequence([
-            Animated.timing(
-               this.state.LarAnimada,
-               { toValue: 200, duration: 700, useNativeDriver: false }
-            ),
-            Animated.timing(
-               this.state.LarAnimada,
-               { toValue: 150, duration: 700, useNativeDriver: false }
-            ),
-         
-         ])
-      
-      ).start()
+   }
+  
+   carregarGrafico() {
 
-      
-      
+
    }
   
     
    render() {
       return (
       <View style={estilos.container} >
-         <Animated.View style={{ width: this.state.LarAnimada, height: this.state.AltAnimada, backgroundColor: '#4169F1', justifyContent: 'center', borderRadius: 25 }}>
-            <Text style={{ color: '#FFFFFF', fontSize: 22, textAlign: 'center' }} >Carregando...</Text>
-         </Animated.View>
+         <View style={estilos.a1} >
+            <TouchableOpacity onPress={this.carregarGrafico} >
+               <Text style={estilos.a2} >
+                  Gerar gráfico...
+               </Text>
+            </TouchableOpacity>
+         </View>
+         <View style={estilos.a3} >
+            <Animated.View style={{ width: this.state.LarAnimada, height: this.state.AltAnimada, backgroundColor: '#4169F1', justifyContent: 'center', borderRadius: 25 }}>
+               <Text style={{ color: '#FFFFFF', fontSize: 22, textAlign: 'center', opacity: this.state.OpAnimada }} >Carregando...</Text>
+            </Animated.View>
+
+         </View>
       </View>
 
       )
@@ -50,8 +50,18 @@ class App extends Component {
 
 const estilos = StyleSheet.create({
    container: {
-      flex: 1, justifyContent: 'center', alignItems: 'center'
+      flex: 1
+   },
+   a1: {
+      height: 80, alignItems: 'center', justifyContent: 'center', flexDirection: 'row', backgroundColor: '#4169E1'
+   },
+   a2: {
+      fontSize: 25, color: '#FFFFFF'
+   },
+   a3: {
+      flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center'
    }
+   
    
 
 })
