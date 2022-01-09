519a96a288b9191757cd50396d793a1e6ccbbc9f
codingscode <heronnestg@hotmail.com>
2022-01-08 22:50:08 -0300

    parte 154+

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,68 +1,17 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
-import AsyncStorage from '@react-native-async-storage/async-storage'
+
 
 
 
 function App() {
-   const [nome, setNome] = useState('')
-   const [entrada, setEntrada] = useState('')
-   const nomeEntrada = useRef(null)
    
 
-
-   //component didmount
-   useEffect(() => {
-      async function acessarStorage() {
-         const nomeStorage = await AsyncStorage.getItem('nomes')
-         if (nomeStorage !== null) {
-            setNome(nomeStorage)
-         }
-      }
-
-      acessarStorage()
-
-   }, [])
-
-
-   //component didupdate
-   useEffect(()=>{
    
-      async function salvarStorage() {
-         await AsyncStorage.setItem('nomes', nome)
-      }
-      
-      salvarStorage()
-      
-   }, [nome])
-   
-   function alterarNome() {
-      setNome(entrada)
-      setEntrada('')
-   }
-   
-   function novoNome() {
-      nomeEntrada.current.focus()
-   }
-   
-   const letrasNome = useMemo(() => {
-      console.log('Mudou letra')
-      return nome.length
-   }, [nome])
-  
-
    return (
       <View style={estilos.container} >
-         <TextInput placeholder="Seu nome..." value={entrada} onChangeText={(valor) => setEntrada(valor)} ref={nomeEntrada} />
-         <TouchableOpacity style={estilos.botao} onPress={alterarNome} >
-            <Text style={estilos.botaoTexto} >Alterar nome</Text>
-         </TouchableOpacity>
-         <Text style={estilos.texto} >{ nome }</Text>
-         <Text style={estilos.texto} >Tem { letrasNome } letras</Text>
-         <TouchableOpacity onPress={novoNome} >
-            <Text>Novo Nome</Text>
-         </TouchableOpacity>
+         <Text>oie</Text>
          
       </View>
 
@@ -73,15 +22,6 @@ function App() {
 const estilos = StyleSheet.create({
    container: {
       flex: 1, marginTop: 15
-   },
-   texto: {
-      color: '#000', fontSize: 35
-   },
-   botao: {
-      backgroundColor: '#222', alignItems: 'center'
-   },
-   botaoTexto: {
-      color: '#FFF'
    }
    
    
