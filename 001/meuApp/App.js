87f047836b02b2d1887b53e8fcec572880089e2c
codingscode <heronnestg@hotmail.com>
2022-01-08 23:30:05 -0300

    parte 156

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -17,6 +17,14 @@ function App() {
             <Text style={estilos.texto} >Digite o cep desejado</Text>
             <TextInput style={estilos.entrada} placeholder="Ex: 79003241" value={cep} onChangeText={ (texto) => setCep(texto) } keyboardType="numeric" />
          </View>
+         <View style={estilos.areaBtn} >
+            <TouchableOpacity style={[estilos.botao, {backgroundColor: 'blue'}]} >
+               <Text style={estilos.botaoTexto} >Buscar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[estilos.botao, {backgroundColor: 'orange'}]} >
+               <Text style={estilos.botaoTexto} >Limpar</Text>
+            </TouchableOpacity>
+         </View>
          
       </SafeAreaView>
 
@@ -36,6 +44,15 @@ const estilos = StyleSheet.create({
    },
    entrada: {
       backgroundColor: '#FFF', borderWidth: 1, borderColor: '#DDD', borderRadius: 5, width: '90%', padding: 10, fontSize: 18
+   },
+   areaBtn: {
+      alignItems: 'center', flexDirection: 'row', marginTop: 15, justifyContent: 'space-around'
+   },
+   botao: {
+      height: 60, justifyContent: 'center', alignItems: 'center', padding: 15, borderRadius: 5
+   },
+   botaoTexto: {
+      fontSize: 22, color: '#FFFF'
    }
    
    
