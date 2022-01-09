afb94f1cfd235ef7a4475eb2c3a1190820bb59a8
codingscode <heronnestg@hotmail.com>
2022-01-08 23:11:47 -0300

    parte 155

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, SafeAreaView } from 'react-native'
 
+import api from './src/services/api'
 
 
 
 
 function App() {
    
-
+   const [cep, setCep] = useState('')
    
+
    return (
-      <View style={estilos.container} >
-         <Text>oie</Text>
+      <SafeAreaView style={estilos.container} >
+         <View style={estilos.a1} >
+            <Text style={estilos.texto} >Digite o cep desejado</Text>
+            <TextInput style={estilos.entrada} placeholder="Ex: 79003241" value={cep} onChangeText={ (texto) => setCep(texto) } keyboardType="numeric" />
+         </View>
          
-      </View>
+      </SafeAreaView>
 
    )
 
@@ -21,7 +26,16 @@ function App() {
 
 const estilos = StyleSheet.create({
    container: {
-      flex: 1, marginTop: 15
+      flex: 1
+   },
+   a1: {
+      alignItems: 'center'
+   },
+   texto: {
+      marginTop: 25, marginBottom: 15, fontSize: 25, fontWeight: 'bold'
+   },
+   entrada: {
+      backgroundColor: '#FFF', borderWidth: 1, borderColor: '#DDD', borderRadius: 5, width: '90%', padding: 10, fontSize: 18
    }
    
    
