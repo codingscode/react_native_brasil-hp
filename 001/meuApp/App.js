c0c59316390ed5945530b93127076138ade3302b
codingscode <heronnestg@hotmail.com>
2022-01-11 21:13:52 -0300

    parte 166

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import Feather from 'react-native-vector-icons/Feather'
 
 
 
@@ -13,6 +14,13 @@ function App() {
       <View style={estilos.container} >
          <Text>oie</Text>
          <FontAwesome name="home" size={45} color="#11118c" />
+         <FontAwesome name="user" size={45} color="green" />
+         <Feather name="gift" size={65} color="purple" />
+         <TouchableOpacity style={estilos.botaoYT} >
+            <FontAwesome name="youtube" size={35} color="red" />
+            <Text style={estilos.botaoTexto} >Acessar canal</Text>
+         </TouchableOpacity>
+         
       </View>
 
    )
@@ -22,6 +30,12 @@ function App() {
 const estilos = StyleSheet.create({
    container: {
       flex: 1, justifyContent: 'center', alignItems: 'center'
+   },
+   botaoYT: {
+
+   },
+   botaoTexto: {
+      
    }
   
    
