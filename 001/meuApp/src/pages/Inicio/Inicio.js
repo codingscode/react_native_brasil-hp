9e1b194a0076df00ff90466e688a10769dc38829
codingscode <heronnestg@hotmail.com>
2022-01-17 11:06:14 -0300

    parte 182

--- 001/meuApp/src/pages/Inicio/Inicio.js
+++ 001/meuApp/src/pages/Inicio/Inicio.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
 
 
 function Inicio() {
 
+   const navegacao = useNavigation()
    
    return (
        <View>
            <Text>Inicio</Text>
+           <Button title="Contato" onPress={ () => navegacao.navigate('Contato') } />
        </View>
    )
 
