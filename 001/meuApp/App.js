7657805d022bf1fbe63aa2113ce8b820e2d53882
codingscode <heronnestg@hotmail.com>
2022-01-18 22:19:37 -0300

    parte 187

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from  '@react-navigation/native-stack' //'@react-navigation/stack'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 
@@ -12,6 +13,9 @@ import Contato from './src/pages/Contato/Contato'
 
 
 const Tab = createBottomTabNavigator()
+
+const Stack = createNativeStackNavigator()
+
 
 const icones = {
    Inicio: {
@@ -27,37 +31,44 @@ const icones = {
 }
 
 
-function App() {
+function Abas() {
    
    
 
    return (
-      <NavigationContainer>
-         <Tab.Navigator 
-              screenOptions={ ({route}) => ({
-                 tabBarStyle: { backgroundColor: 'purple' },
-                 tabBarActiveTintColor: '#FFF',
-                 tabBarInactiveTintColor: 'yellow',
-                 tabBarIcon: ({ color, size }) => {
-                    const { name } = icones[route.name]
-                    return <Icon name={name} color={color} size={size} />
-                    
-                 }
-              })}
-              
-          >
-            <Tab.Screen name="Inicio" component={Inicio} />
-            <Tab.Screen name="Sobre" component={Sobre} />
-            <Tab.Screen name="Contato" component={Contato} />
-         </Tab.Navigator>
-        
-      </NavigationContainer>
+      
+      <Tab.Navigator 
+           screenOptions={ ({route}) => ({
+              tabBarStyle: { backgroundColor: 'purple' },
+              tabBarActiveTintColor: '#FFF',
+              tabBarInactiveTintColor: 'yellow',
+              tabBarIcon: ({ color, size }) => {
+                 const { name } = icones[route.name]
+                 return <Icon name={name} color={color} size={size} />
+                 
+              }
+           })}
+           
+       >
+         <Tab.Screen name="Inicio" component={Inicio} />
+         <Tab.Screen name="Sobre" component={Sobre} />
+      </Tab.Navigator>
       
    )
 
 }
 
+function App() {
 
+   return (
+      <NavigationContainer>
+         <Stack.Navigator>
+            <Stack.Screen name="Inicio" component={Abas} />
+            <Stack.Screen name="Contato" component={Contato} />
+         </Stack.Navigator>
+      </NavigationContainer>
+   )
+}
 
 
 export default App
