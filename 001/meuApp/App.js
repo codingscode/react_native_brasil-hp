b2d43e99e68ee7642f751fed5fdbdc0201abaa1b
codingscode <heronnestg@hotmail.com>
2022-01-25 12:31:41 -0300

    parte 192

--- 001/meuApp/App.js
+++ 001/meuApp/App.js
@@ -11,6 +11,7 @@ const Drawer = createDrawerNavigator()
 import Inicio from './src/pages/Inicio/Inicio'
 import Sobre from './src/pages/Sobre/Sobre'
 import Contato from './src/pages/Contato/Contato'
+import DrawerCustomizado from './src/components/DrawerCustomizado'
 
 
 
@@ -18,10 +19,10 @@ function App() {
    
    return (
       <NavigationContainer>
-         <Drawer.Navigator>
-            <Drawer.Screen name="Inicio" component={Inicio} options={{ headerShown: false }} />
-            <Drawer.Screen name="Sobre" component={Sobre} options={{ headerShown: false }} />
-            <Drawer.Screen name="Contato" component={Contato} options={{ headerShown: false }} />
+         <Drawer.Navigator drawerContent={DrawerCustomizado} >
+            <Drawer.Screen name="Inicio" component={Inicio} />
+            <Drawer.Screen name="Sobre" component={Sobre} />
+            <Drawer.Screen name="Contato" component={Contato} />
          </Drawer.Navigator>
       </NavigationContainer>
    )
---
+++ 001/meuApp/src/components/DrawerCustomizado.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import { View, Text } from 'react-native'
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
+
+
+
+export default function DrawerCustomizado() {
+
+   return (
+      <DrawerContentScrollView>
+         <Text>Olá mundo</Text>
+      </DrawerContentScrollView>
+   )
+}
+
+
+
+
+
+
