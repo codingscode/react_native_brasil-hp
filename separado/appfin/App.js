379f3c2b7164391a0cc8614cd6b4f6f90599564c
codingscode <heronnestg@hotmail.com>
2023-03-16 12:20:28 -0300

    separado parte 1

---
+++ separado/appfin/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+
+function App() {
+  
+
+   return (
+      <View style={estilos} >
+        <Text>Criando App</Text>
+      </View>
+   )
+}
+
+const estilos = StyleSheet.create({
+  container: {
+     flex: 1, backgroundColor: '#fff'
+  }
+})
+
+
+export default App
