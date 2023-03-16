8e11e4b45787b1074a54f808757f0b2518d147c4
codingscode <heronnestg@hotmail.com>
2023-03-15 23:01:11 -0300

    parte 198

--- 003/maisApp/App.js
+++ 003/maisApp/App.js
@@ -1,24 +1,30 @@
 
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import styled from 'styled-components/native'
 
 
 function App() {
    
    return (
-      <View style={estilos.container} >
-         <Text>Sujeito Programador</Text>
-      </View>
+      <Container  >
+         <Titulo>Sujeito Programador</Titulo>
+      </Container>
    )
 }
 
+const Container = styled.View`
+   flex: 1;
+   justify-content: center;
+   align-items: center;
+   background-color: #FF0000;
 
+`
 
-const estilos = StyleSheet.create({
-   container: {
-      flex: 1, justifyContent: 'center', alignItems: 'center'
-   }
-})
+const Titulo = styled.Text`
+   color: lightgreen;
+   font-size: 25px;
+`
 
 
 export default App
