672210ae4c7d3042a2a9f9a0167b960234d0a1bb
codingscode <heronnestg@hotmail.com>
2023-03-16 11:38:54 -0300

    parte 203

---
+++ 004/camera/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+import { View, Text } from 'react-native'
+
+
+
+function App() {
+  
+   return (
+      <View>
+         <Text>Oi</Text>
+      </View>
+   )
+}
+
+
+export default App
