6a3b0940d8d2f71f0d73a9d4dbe02aec37e13972
codingscode <heronnestg@hotmail.com>
2023-03-16 22:38:36 -0300

    separado parte 4

--- separado/appfin/src/components/Header/index.js
+++ separado/appfin/src/components/Header/index.js
@@ -16,9 +16,9 @@ function Header() {
     return (
       <View style={estilos.container} >
          <View style={estilos.content} >
-            <Text>Sujeito Programador</Text>
+            <Text style={estilos.username} >Sujeito Programador</Text>
 
-            <TouchableOpacity activeOpacity={0.9} >
+            <TouchableOpacity activeOpacity={0.9} style={estilos.buttonUser} >
                <Image source={require('../../images/user.png')} style={estilos.imagem} />
             </TouchableOpacity>
          </View>
@@ -30,13 +30,26 @@ const estilos = StyleSheet.create({
     container: {
         backgroundColor: '#8000ff',
         paddingTop: statusBarHeight,
+        flexDirection: 'row',
+        paddingStart: 16,
+        paddingEnd: 16,
+        paddingBottom: 44
     },
     imagem: {
         width: 50,
         height: 50,
-        
-        
+                
     },
+    content: {
+        flex: 1, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'
+    },
+    username: {
+        fontSize: 18, color: 'white', fontWeight: 'bold'
+    },
+    buttonUser: {
+        width: 44, height: 44, backgroundColor: 'rgba(255, 255, 255, 0.5)', justifyContent: 'center', alignItems: 'center', borderRadius: 22
+    }
+
 })
 
 
