774239393c3860dce1b864a0157d71074215a094
codingscode <heronnestg@hotmail.com>
2023-03-16 22:23:31 -0300

    separado parte 3

--- separado/appfin/src/components/Header/index.js
+++ separado/appfin/src/components/Header/index.js
@@ -1,19 +1,42 @@
 
 import React from 'react'
-import { View, StyleSheet, Text, StatusBar } from 'react-native'
+import { View, StyleSheet, Image, Text, StatusBar, TouchableOpacity } from 'react-native'
+
+
+
+
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64
+
+/*
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
+*/
 
 function Header() {
-   return (
+    return (
       <View style={estilos.container} >
-         <Text>Header do app</Text>
+         <View style={estilos.content} >
+            <Text>Sujeito Programador</Text>
+
+            <TouchableOpacity activeOpacity={0.9} >
+               <Image source={require('../../images/user.png')} style={estilos.imagem} />
+            </TouchableOpacity>
+         </View>
       </View>
    )
 }
 
 const estilos = StyleSheet.create({
-   container: {
-      backgroundColor: '#8000ff'
-   }
+    container: {
+        backgroundColor: '#8000ff',
+        paddingTop: statusBarHeight,
+    },
+    imagem: {
+        width: 50,
+        height: 50,
+        
+        
+    },
 })
 
 
