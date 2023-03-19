ff4e75874f862160bfc2105a0599988ef42dbcc1
codingscode <heronnestg@hotmail.com>
2023-03-18 22:05:15 -0300

    separado parte 14

--- separado/appfin/src/components/Movements/index.js
+++ separado/appfin/src/components/Movements/index.js
@@ -14,8 +14,8 @@ function Movements({ data }) {
             <Text style={estilos.label} >
                {data.label}
             </Text>
-            <Text style={estilos.valor} >
-               {data.valor}
+            <Text style={data.tipo === 1 ? estilos.valor : estilos.expenses} >
+               R$ {data.valor}
             </Text>
 
          </View>
@@ -26,9 +26,36 @@ function Movements({ data }) {
 
 const estilos = StyleSheet.create({
    container: {
-      flex: 1, backgroundColor: '#fafafa'
-   },
+      flex: 1,
+      marginBottom: 24,
+      borderBottomWidth: 0.5,
+      borderBottomColor: '#DADADA'
 
+   },
+   content: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 2,
+      marginBottom: 8
+   },
+   data: {
+      color: '#DADADA',
+      fontWeight: 'bold'
+   },
+   label: {
+      fontWeight: 'bold',
+      fontSize: 16
+   },
+   valor: {
+      fontSize: 16,
+      color: '#2ecc71',
+      fontWeight: 'bold'
+   },
+   expenses: {
+      fontSize: 16,
+      color: '#e74c3c',
+      fontWeight: 'bold'
+   }
    
 
 })
